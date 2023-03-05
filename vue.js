f98918736784494ee44e584c43946d333b1c6ce3
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-03-05 16:02:03 +0100

    Attribute Binding

--- vue.js
+++ vue.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.google.com",
       title: "hello world",
-      age: 12
+      show: true,
+      age: 12,
+      users: [
+        { name: "jack", age: 12 },
+        { name: "tom", age: 13 },
+        { name: "jerry", age: 14 }
+      ]
     }
+  },
+  methods: {
+    changeTitle() {
+      this.title = "hello vue"
+    },
+    showToggles() {
+      this.show = !this.show
+    }
+
   }
 })
 app.mount("#app")
